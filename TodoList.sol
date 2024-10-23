16092fd567296af8fab2afce6a3514cd3712df74
yaasiinaxmed <yaskassoy@gmail.com>
2024-10-23 10:40:06 +0300

    Function to mark a task as completed for the sender

--- TodoList.sol
+++ TodoList.sol
@@ -2,26 +2,34 @@
 pragma solidity ^0.8.4;
 
 contract TodoList {
-    uint public taskCount = 0;
+    uint256 public taskCount = 0;
 
     struct Task {
-        uint id;
+        uint256 id;
         string content;
         bool completed;
     }
 
     // Mapping from user address to their tasks
-    mapping(address => mapping(uint => Task)) public tasks;
-    mapping(address => uint) public userTaskCount;
+    mapping(address => mapping(uint256 => Task)) public tasks;
+    mapping(address => uint256) public userTaskCount;
 
-    event TaskCreated(address user, uint id, string content, bool completed);
-    event TaskCompleted(address user, uint id, bool completed);
+    event TaskCreated(address user, uint256 id, string content, bool completed);
+    event TaskCompleted(address user, uint256 id, bool completed);
 
     // Function to create a new task for the sender
     function createTask(string memory _content) public {
         userTaskCount[msg.sender]++;
-        uint newTaskId = userTaskCount[msg.sender];
+        uint256 newTaskId = userTaskCount[msg.sender];
         tasks[msg.sender][newTaskId] = Task(newTaskId, _content, false);
         emit TaskCreated(msg.sender, newTaskId, _content, false);
+    }
+
+    // Function to mark a task as completed for the sender
+    function toggleTaskCompleted(uint256 _id) public {
+        Task storage task = tasks[msg.sender][_id];
+        require(task.id != 0, "Task not found"); //Ensure task exists
+        task.completed = !task.completed;
+        emit TaskCompleted(msg.sender, _id, task.completed);
     }
 }
